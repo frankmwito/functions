fb04c5669ef7b33d44d581cd8da4337cc22c62f5
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-28 10:49:53 +0300

    return uppercase in a given list

--- convertUppercase.py
+++ convertUppercase.py
@@ -1,1 +1,14 @@
 # accept a list of strings and return a new list with all strings converted to uppercase
+
+def uppercase(words):
+    return [word.upper() for word in words]    
+
+num = int(input("Enter the size of your list:"))
+words = list()
+
+for i in range(num):
+    words.append(input(f"Enter any word {i+1}:"))
+    
+upper_case = uppercase(words)
+
+print(f"The uppercse of the list is: {upper_case}")
