1a0331e81997fc6f761e623ed3ea8a6cfcdf0aac
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-28 10:24:59 +0300

    check if two strings are anagrams of each other

--- Anagrams.py
+++ Anagrams.py
@@ -1,1 +1,10 @@
 # function that takes two strings as arguments and returns true if the strings are anagrams, otherwise false
+
+def anagrams_check(word1, word2):
+    isAnagram = False
+    if sorted(word1) == sorted(word2):
+        isAnagram = True
+    return isAnagram
+
+result = anagrams_check(input("Enter first word: "), input("Enter second word: "))
+print(result)
